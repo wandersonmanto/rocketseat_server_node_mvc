a82f0159c1e6d17069d263173a016e7684b95f9c
Wanderson Barbosa <wandersonmanto@gmail.com>
2024-05-30 23:42:51 -0300

    add routes in src

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 
+const routes = require('./routes')
+
 const app = express()
 app.use(express.json())
+
+app.use(routes)
 
 app.get('/message/:id/:user', (request, response) => {
   const { id, user } = request.params
@@ -16,12 +20,6 @@ app.get('/users', (request, response) => {
   const { page, limit } = request.query
 
   response.send(`Página: ${page}. Mostrar: ${limit}`)
-})
-
-app.post('/users', (request, response) => {
-  const { name, email, password } = request.body
-
-  response.json({ name, email, password })
 })
 
 const PORT = 3333
