720500d7b3d219eb94b2f0ef441fb24f5157ecc9
Wanderson Barbosa <wandersonmanto@gmail.com>
2024-05-30 23:29:44 -0300

    post body params

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 
 const app = express()
+app.use(express.json())
 
 app.get('/message/:id/:user', (request, response) => {
   const { id, user } = request.params
@@ -15,6 +16,12 @@ app.get('/users', (request, response) => {
   const { page, limit } = request.query
 
   response.send(`Página: ${page}. Mostrar: ${limit}`)
+})
+
+app.post('/users', (request, response) => {
+  const { name, email, password } = request.body
+
+  response.json({ name, email, password })
 })
 
 const PORT = 3333
