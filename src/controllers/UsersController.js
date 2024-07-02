cbad0bf53233693349748371ae8b8736e4aea016
Wanderson Barbosa <wandersonmanto@gmail.com>
2024-07-02 16:18:54 -0300

    src/utils AppError

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,9 +1,15 @@
+const AppError = require("../utils/AppError")
+
 class UsersController {
 
   create(request, response) {
     const { name, email, password } = request.body
 
-    response.json({ name, email, password })
+    if(!name) {
+      throw new AppError("Nome é obrigatório!")
+    }
+
+    response.status(201).json({ name, email, password })
   }
   /**
    * index - GET para listar vários registros.
