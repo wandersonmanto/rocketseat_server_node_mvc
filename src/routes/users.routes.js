f14626d4cd058b8b5e79f9d14e07071c150db5e0
Wanderson Barbosa <wandersonmanto@gmail.com>
2024-07-02 16:11:35 -0300

    middleware

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -5,12 +5,16 @@ const UsersController = require('../controllers/UsersController')
 const usersRoutes = Router()
 
 function myMiddleware(request, response, next) {
-  
-}
+  console.log("myMiddleware - usersController")
+  console.log(request.body)
 
+  next();
+}
 
 const usersController = new UsersController()
 
-usersRoutes.post('/', usersController.create)
+// para usar um Middleware em todas as rotas.
+// usersRoutes.use(myMiddleware)
+usersRoutes.post('/', myMiddleware ,usersController.create)
 
 module.exports = usersRoutes
